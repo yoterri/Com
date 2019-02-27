0ca91b5f12da4e1e285e8bddaa582d1cc15d294f
Yassir Terri Mejia <yoterri@ideasti.com>
2019-02-27 17:16:56 -0400

    update

---
+++ src/Validator/Db/RecordExists.php
@@ -0,0 +1,69 @@
+<?php
+namespace Com\Validator\Db;
+
+use Zend\Validator\Exception;
+
+/**
+ * Confirms a record exists in a table.
+ */
+class RecordExists extends \Zend\Validator\Db\RecordExists
+{
+
+    protected $byPassValue = null;
+    protected $hasByPassValue = false;
+
+
+    /**
+     * @param mixed $valule
+     * @return ClosureNodeExists
+     */
+    function setByPassValue($value)
+    {
+        $this->byPassValue = $value;
+        $this->hasByPassValue = true;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    function getByPassValue()
+    {
+        return $this->byPassValue;
+    }
+
+    /**
+     * @return ClosureNodeExists
+     */
+    function removeByPassValue()
+    {
+        $this->hasByPassValue = false;
+        return $this;
+    }
+
+
+    public function isValid($value)
+    {
+        if($this->hasByPassValue)
+        {
+            if(is_array($this->byPassValue))
+            {
+                $flag = in_array($value, $this->byPassValue);
+                if($flag)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                $flag = ($value == $this->byPassValue);
+                if($flag)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return parent::isValid($value);
+    }
+}
