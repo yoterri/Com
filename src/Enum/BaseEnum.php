55717b89c4610b4e45652c68e7c6456f8d4a062a
Yassir Terri Mejia <yoterri@ideasti.com>
2019-10-18 09:24:39 -0400

    small fix

--- src/Enum/BaseEnum.php
+++ src/Enum/BaseEnum.php
@@ -16,7 +16,7 @@ class BaseEnum implements LazyLoadInterface
 
 
     protected $status = array(
-    	self::ENABLED => 'Enabled',
+        self::ENABLED => 'Enabled',
         self::DISABLED => 'Disabled',
     );
 
@@ -33,30 +33,73 @@ class BaseEnum implements LazyLoadInterface
 
 
 
-	function getStatus($key = null)
+    function getStatus($key = null)
     {
-    	return $this->_get($this->status, $key);
+        return $this->_get($this->status, $key);
     }
 
 
-	protected function _get(array $values, $key = null)
+    /**
+     * @param string $property - property name
+     * @param string $key
+     * @return array | string
+     */
+    function get($property, $key = null)
     {
-    	if(empty($key))
-    	{
-    		return $values;
-    	}
-    	else
-    	{
-    		if(!isset($values[$key]))
-    		{
-    			throw new \Exception("Key '$key' value not found");
-    		}
-    		else
-    		{
-    			return $values[$key];
-    		}
-    	}
+        if( !isset($this->$property) )
+        {
+            $p = htmlentities($property);
+            throw new \Exception("Property '$p' was not found.");
+        }
+
+        if( !is_array($this->$property) )
+        {
+            $p = htmlentities($property);
+            throw new \Exception("Property '$p' is not  avalid array.");
+        }
+
+        return $this->_get($this->$property, $key);
     }
 
+
+    /**
+     * @param string $property - property name
+     * @return bool
+     */
+    function propertyExists($property)
+    {
+        return isset($this->$property);
+    }
+
+
+    /**
+     * @param string $property - property name
+     * @param string $key
+     * @return bool
+     */
+    function keyExists($property, $key)
+    {
+        return $this->propertyExists($property) && isset($this->$property[$key]);
+    }
+
+
+    protected function _get(array $values, $key = null)
+    {
+        if(empty($key))
+        {
+            return $values;
+        }
+        else
+        {
+            if(!isset($values[$key]))
+            {
+                throw new \Exception("Key '$key' value not found");
+            }
+            else
+            {
+                return $values[$key];
+            }
+        }
+    }
 }
 
