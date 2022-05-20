6f4a3e83a9e8d09eee9aa38ac8828c0574561525
Yassir Terri Mejia <yoterri@ideasti.com>
2022-05-20 19:29:59 -0400

    small fix

--- src/Model/QuerySelect.php
+++ src/Model/QuerySelect.php
@@ -53,8 +53,7 @@ class QuerySelect extends AbstractModel
      */
     function getSelect()
     {
-        if(!$this->select) 
-        {
+        if (!$this->select)  {
             $this->prepare();
         }
 
@@ -62,14 +61,18 @@ class QuerySelect extends AbstractModel
     }
 
 
+    
+
+
     /**
      * @param AbstractEntity $entity
+     * @param string|Laminas\Db\Adapter\Adapter $dbAdapter
      * @return ResultSet
      */
-    function getResult(AbstractEntity $entity = null)
+    function getResult(AbstractEntity $entity = null, $dbAdapter = null)
     {
-        $adapter = $this->getDbAdapter();
-        $sql = new Sql($adapter);
+        $dbAdapter = $this->_getAdapter($dbAdapter);
+        $sql = new Sql($dbAdapter);
 
         $select = $this->getSelect();
         $statement = $sql->prepareStatementForSqlObject($select);
@@ -78,8 +81,7 @@ class QuerySelect extends AbstractModel
         // build result set
         $resultSet = new ResultSet();
         
-        if(is_null($entity))
-        {
+        if (is_null($entity)) {
             $entity = $this->getContainer()->get('Com\Entity\Record');
         }
         
@@ -94,39 +96,20 @@ class QuerySelect extends AbstractModel
     /**
      * @param int $pageNumber
      * @param int $itemsPerPage
-     * @param string|Laminas\Db\Adapter\Adapter $adapter
+     * @param string|Laminas\Db\Adapter\Adapter $dbAdapter
      * @return Paginator
      */
-    function getPaginator($pageNumber = null, $itemsPerPage = null, $adapter = null)
+    function getPaginator($pageNumber = null, $itemsPerPage = null, $dbAdapter = null)
     {
-        if(!empty($adapter))
-        {
-            $sm = $this->getContainer();
-
-            if(is_string($adapter))
-            {
-                $dbAdapter = $sm->get($adapter);
-            }
-            else
-            {
-                $dbAdapter = $dbAdapter;
-            }
-        }
-        else
-        {
-            $dbAdapter = $this->getDbAdapter();
-        }
-
+        $dbAdapter = $this->_getAdapter($dbAdapter);
         $paginatorAdapter = new DbSelect($this->getSelect(), $dbAdapter);
         $paginator = new Paginator($paginatorAdapter);
 
-        if(!is_null($pageNumber))
-        {
+        if (!is_null($pageNumber)) {
             $paginator->setCurrentPageNumber($pageNumber);
         }
 
-        if(!is_null($itemsPerPage))
-        {
+        if (!is_null($itemsPerPage)) {
             $paginator->setItemCountPerPage($itemsPerPage);
         }
 
@@ -142,5 +125,55 @@ class QuerySelect extends AbstractModel
             default:
                 throw new \Exception('Not a valid magic property for this object');
         }
+    }
+
+
+
+    /**
+     *
+     * @param string $exit Indica si se debe detener la ejecucion del código
+     * @param string|Laminas\Db\Adapter\Adapter $dbAdapter
+     */
+    function debugSql($exit = true, $dbAdapter = null)
+    {
+        $str = $this->getSqlAsString($dbAdapter);
+        
+        echo '<pre>';
+        echo $str;
+        echo '</pre>';
+        
+        if ($exit) {
+            exit;
+        }
+    }
+
+
+    /**
+     * @param string|Laminas\Db\Adapter\Adapter $dbAdapter
+     * @return string
+     */
+    function getSqlAsString($dbAdapter = null)
+    {
+        $dbAdapter = $this->_getAdapter($dbAdapter);
+        $sql = $this->getSelect();
+        $str = $sql->getSqlString($dbAdapter->getPlatform());
+        
+        return "$str";
+    }
+
+
+
+    protected function _getAdapter($dbAdapter = null)
+    {
+        if (empty($dbAdapter)) {
+            $dbAdapter = $this->getDbAdapter();
+        } else {
+            if (is_string($dbAdapter)) {
+                $sm = $this->getContainer();
+                $dbAdapter = $sm->get($dbAdapter);
+            }
+        }
+
+        return $dbAdapter;
     }
 }
